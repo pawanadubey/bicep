5ff6c82ceb823743bb44ee5c3636d0d6d41983db
Marcin Jastrzebski <majastrz@microsoft.com>
2021-01-26 19:15:05 -0800

    compiles

--- src/Bicep.Core/TypeSystem/TypeAssignmentVisitor.cs
+++ src/Bicep.Core/TypeSystem/TypeAssignmentVisitor.cs
@@ -135,12 +135,13 @@ namespace Bicep.Core.TypeSystem
                     diagnostics.Write(DiagnosticBuilder.ForPosition(syntax.Type).ResourceTypesUnavailable(resourceType.TypeReference));
                 }
 
-                if (syntax.IfCondition is IfConditionSyntax ifConditionSyntax)
+                if (syntax.Value is IfConditionSyntax ifConditionSyntax)
                 {
                     diagnostics.WriteMultiple(this.ValidateIfCondition(ifConditionSyntax));
+                    return TypeValidator.NarrowTypeAndCollectDiagnostics(typeManager, ifConditionSyntax.Body, declaredType, diagnostics);
                 }
 
-                return TypeValidator.NarrowTypeAndCollectDiagnostics(typeManager, syntax.Body, declaredType, diagnostics);
+                return TypeValidator.NarrowTypeAndCollectDiagnostics(typeManager, syntax.Value, declaredType, diagnostics);
             });
 
         public override void VisitModuleDeclarationSyntax(ModuleDeclarationSyntax syntax)
@@ -163,12 +164,13 @@ namespace Bicep.Core.TypeSystem
                     diagnostics.Write(DiagnosticBuilder.ForPosition(syntax.Path).ReferencedModuleHasErrors());
                 }
 
-                if (syntax.IfCondition is IfConditionSyntax ifConditionSyntax)
+                if (syntax.Value is IfConditionSyntax ifConditionSyntax)
                 {
                     diagnostics.WriteMultiple(this.ValidateIfCondition(ifConditionSyntax));
+                    return TypeValidator.NarrowTypeAndCollectDiagnostics(typeManager, ifConditionSyntax.Body, declaredType, diagnostics);
                 }
                 
-                return TypeValidator.NarrowTypeAndCollectDiagnostics(typeManager, syntax.Body, declaredType, diagnostics);
+                return TypeValidator.NarrowTypeAndCollectDiagnostics(typeManager, syntax.Value, declaredType, diagnostics);
             });
 
         public override void VisitParameterDeclarationSyntax(ParameterDeclarationSyntax syntax)
